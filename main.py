8ff903c4364eec4cf9be5a58e9e9bbee9063c2a0
Rustam Shangareev <rustam.letitbe@gmail.com>
2024-04-11 23:43:09 +0300

    Migrate sarcasm_classifier to classifiers module

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_extras.badges import badge
-from sarcasm_classifier import SarcasmClassifier
+from classifiers.sarcasm_classifier import SarcasmClassifier
 
 def main_page(classifier):
     st.subheader("Main page")
